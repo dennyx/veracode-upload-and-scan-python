729a14e6704284c5c2ec9cc95488d59e236b6986
Chris C <chris@ctcampbell.com>
2017-07-05 18:17:38 +0100

    Updated run.py

--- run.py
+++ run.py
@@ -1,14 +1,27 @@
-# run.py id key app_profile scan_name sandbox
-
+import argparse
 import sys
 import subprocess
-import datetime
 import time
+import logging
 
 
-wait = 10
-total_wait = 3600
+# args
 
+parser = argparse.ArgumentParser()
+parser.add_argument('api_wrapper_jar', help='file path to Veracode API Java wrapper')
+parser.add_argument('id', help='Veracode API credentials ID')
+parser.add_argument('key', help='Veracode API credentials key')
+parser.add_argument('appname', help='Veracode application profile name')
+parser.add_argument('version', help='Scan name')
+group = parser.add_mutually_exclusive_group()
+group.add_argument('--sandboxname', help='Veracode sandbox name')
+group.add_argument('--break_the_build', action="store_true", help='Break the build if scan policy compliance fails')
+parser.add_argument('--wait_interval', type=int, default=30, help='Time interval (s) between scan status checks, default = 30s')
+parser.add_argument('--wait_max', type=int, default=3600, help='Maxiumum time (s) to wait for scan to complete, default = 1h')
+args = parser.parse_args()
+
+
+# helpers
 
 def get_substring(s, leader, trailer):
     end_of_leader = s.index(leader) + len(leader)
@@ -16,45 +29,27 @@ def get_substring(s, leader, trailer):
     return s[end_of_leader:start_of_trailer]
 
 
-def log_now():
-    return '['+datetime.datetime.now().strftime("%y.%m.%d %H:%M:%S")+'] '
+# setup
 
+logging.basicConfig(format='%(asctime)s %(message)s', datefmt='[%y.%m.%d %H:%M:%S]', level=logging.INFO)
 
-if len(sys.argv) >= 5:
-    id = sys.argv[1]
-    key = sys.argv[2]
-    app_profile = sys.argv[3]
-    scan_name = sys.argv[4]
-    if len(sys.argv) >= 6:
-        sandbox = sys.argv[5]
-    else:
-        sandbox = None
-else:
-    pass
+base_command = ['java', '-jar', args.api_wrapper_jar, '-vid', args.id, '-vkey', args.key]
 
-scan_name = scan_name + ' ' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
 
 # upload veracode.zip, possibly to a sandbox
 
-base_command = ['java', '-jar', 'VeracodeJavaAPI.jar',
-                '-vid', id, '-vkey', key]
-command = base_command + ['-action', 'UploadAndScan',
-                          '-createprofile', 'false',
-                          '-appname', app_profile,
-                          '-version', scan_name,
-                          '-filepath', 'veracode.zip']
-if sandbox is not None:
-    command = command + ['-sandboxname', sandbox]
-
-upload = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-stdout = upload.stdout.decode(sys.stdout.encoding)
+command = base_command + ['-action', 'UploadAndScan', '-createprofile', 'false', '-appname', args.appname, '-version', args.version, '-filepath', 'veracode.zip']
+if args.sandboxname:
+    command = command + ['-sandboxname', args.sandboxname]
+upload = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+stdout = upload.communicate()[0]
 print(stdout)
 
 if 'The build_id of the new build is' in stdout:
     try:
         app_id = get_substring(stdout, 'appid=', ')')
         build_id = get_substring(stdout, 'The build_id of the new build is "', '"')
-        if sandbox is not None:
+        if args.sandboxname:
             sandbox_id = get_substring(stdout, 'sandboxid=', ')')
     except ValueError as e:
         print(e)
@@ -62,35 +57,33 @@ if 'The build_id of the new build is' in stdout:
 
     # watch scan status
 
-    command = base_command + ['-action', 'GetBuildInfo',
-                              '-appid', app_id,
-                              '-buildid', build_id]
-    if sandbox is not None:
-        command = command + ['-sandboxid', sandbox_id]
+    if args.break_the_build:
+        command = base_command + ['-action', 'GetBuildInfo', '-appid', app_id, '-buildid', build_id]
 
-    wait_so_far = 0
-    while True:
-        build_info = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        stdout = build_info.stdout.decode(sys.stdout.encoding)
-        print(log_now() + '<build ' + get_substring(stdout, '<build ', '>') + '>')
+        wait_so_far = 0
+        while True:
+            build_info = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            stdout = build_info.communicate()[0]
+            logging.info('<build ' + get_substring(stdout, '<build ', '>') + '>')
 
-        if 'results_ready="true"' in stdout:
-            if sandbox is not None:
-                print(log_now() + 'Scan complete')
-                sys.exit(0)
-
-            if all('policy_compliance_status="' + x not in stdout for x in ['Calculating...', 'Not Assessed']):
-                policy_compliance_status = get_substring(stdout, 'policy_compliance_status="', '"')
-                print(log_now() + 'Scan complete, policy compliance status: ' + policy_compliance_status)
-                if policy_compliance_status in ['Conditional Pass', 'Pass']:
+            if 'results_ready="true"' in stdout:
+                if args.sandboxname:
+                    logging.info('Scan complete in sandbox ' + args.sandboxname)
                     sys.exit(0)
-                else:
-                    sys.exit(1)
 
-        if wait_so_far >= total_wait:
-            print(log_now() + 'Scan did not complete within timeout')
-            sys.exit(1)
-        wait_so_far += wait
-        time.sleep(wait)
+                if all('policy_compliance_status="' + x not in stdout for x in ['Calculating...', 'Not Assessed']):
+                    policy_compliance_status = get_substring(stdout, 'policy_compliance_status="', '"')
+                    logging.info('Scan complete, policy compliance status: ' + policy_compliance_status)
+                    if policy_compliance_status in ['Conditional Pass', 'Pass']:
+                        sys.exit(0)
+                    else:
+                        sys.exit(1)
+
+            if wait_so_far >= args.wait_max:
+                logging.info('Scan did not complete within timeout')
+                sys.exit(1)
+            wait_so_far += args.wait_interval
+            time.sleep(args.wait_interval)
+
 else:
-    sys.exit(500)
+    sys.exit(2)
